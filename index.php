e863f48226f886ce9894261b1ba7fc5f5997cee8
MystAria <adrien.tronchon@gmail.com>
2023-01-20 10:36:02 +0100

    2023 reborn

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="static/levelup_index_style.css" />
+	<link rel="shortcut icon" href="assets/favicon.ico">
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+</head>
+
+<body class="body">
+	<div class="middle">
+		<img src='assets/Title-web2.png' class="logo">
+		<form action="/page2">
+			<div class="bar_form">
+				<input type="text" name="Summoner_Name" required class="input_name" placeholder="Summoner Name">
+				<SELECT name="Region" size="1" class="input_region">
+					<OPTION>EUW
+					<OPTION>NA
+					<OPTION>KR
+					<OPTION>EUN
+					<OPTION>BR
+					<OPTION>JP1     
+					<OPTION>LA1
+					<OPTION>LA2
+					<OPTION>OC
+					<OPTION>RU
+					<OPTION>TR
+				</SELECT>
+				
+				<button class="input_button">Get Stats !</button> 
+			</div>
+		</form>
+		
+		<p class="accueil_text">Level.Up is a personal performance analysis tool for League of Legends.<br>Level.Up easily brings you key statistics and their evolutions through your games to improve on different aspects of the game.</p>
+	</div>
+	
+	<div class="bandeau_bottom">
+		<p class="disclaimer">Level.Up isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</p>
+	</div>
+</body>
+
+</html>
